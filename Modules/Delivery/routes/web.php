cd2fecd14b8b41d0362e5ba5f12a7d720348d333
ElsayedAbdo219 <micro.elsayed219@gmaail.com>
2025-12-20 00:49:22 +0200

    cancel(delivery):cancel permanintly delivery features.

--- Modules/Delivery/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Delivery\Http\Controllers\DeliveryController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('deliveries', DeliveryController::class)->names('delivery');
-});
